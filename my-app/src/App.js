17af12383193f7fc3717553dd70231cf17f1e1fd
praewa rueangsri <rueangsri_p@silpakorn.edu>
2026-08-20 05:04:23 +0000

    Hw-top3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,40 +4,59 @@ import MovieList from "./components/MovieList";
 import Footer from "./components/Footer";
 import Greeting from "./components/Greeting";
 import Card from "./components/Card";
+import ProfileCard from "./components/ProfileCard";
 
-const students = [
-  { id: 1, name: 'ฝน',   year: 3 },
-  { id: 2, name: 'เต้ย', year: 2 },
-  { id: 3, name: 'มายด์', year: 4 },
+import './App.css';
+
+const members = [
+  { id: 1, name: 'แพรวา เรืองศรี', nickname: 'แพรวา',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'แมว'] },
+    { id: 2, name: 'นฤมล เวฬุวิริยกุล', nickname: 'มิ้น',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'แมว'] },
+    { id: 3, name: 'ศุกร์ภชัย ศิริอินภิบาล', nickname: 'ฟลุ๊ค',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['น้ำเปล่า', 'หมา'] },
+  
 ];
 
 function App() {
   return (
-    <div>
-      <Card title="ประกาศ">
-        <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
-        <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
-      </Card>
-      <h1>รายชื่อนักศึกษา</h1>
-      {students.map((s) => (
-        <Greeting key={s.id} name={s.name} year={s.year} />
-      ))}
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
+export default App;
+// const students = [
+//   { id: 1, name: 'ฝน',   year: 3 },
+//   { id: 2, name: 'เต้ย', year: 2 },
+//   { id: 3, name: 'มายด์', year: 4 },
+// ];
 
 // function App() {
 //   return (
 //     <div>
-//       <Header />
-//       <MovieList />
-//       <Greeting name="Praewa" year={3} />
-//       <Greeting name="AA" year={2} />
-//       <Greeting name="PP" year={4} />
-//       <Footer />
+//       <Card title="ประกาศ">
+//         <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
+//         <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
+//       </Card>
+//       <h1>รายชื่อนักศึกษา</h1>
+//       {students.map((s) => (
+//         <Greeting key={s.id} name={s.name} year={s.year} />
+//       ))}
 //     </div>
 //   );
 // }
 
-export default App;
+//export default App;
